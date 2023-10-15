69d2b8e908aa43c0f8c78c0201c910688d17a746
SimeonAleksov <aleksov_s@outlook.com>
2023-10-15 20:31:02 +0200

    Replace direct services usage with usecase

--- infrastructure/cmd/counter.go
+++ infrastructure/cmd/counter.go
@@ -1,8 +1,9 @@
 package cmd
 
 import (
+	"nu/corpus-reader/application/presenter"
 	"nu/corpus-reader/application/repository"
-	"nu/corpus-reader/application/services"
+	"nu/corpus-reader/application/usecase"
 	"nu/corpus-reader/infrastructure/log"
 
 	"github.com/spf13/cobra"
@@ -19,11 +20,20 @@ var counterCommand = &cobra.Command{
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		logger.Infof("Searching for: %v in %v", word, directory)
+		input := usecase.PatternSearchInput{
+			Pattern:   word,
+			Directory: directory,
+		}
 		repo := repository.NewFactory().CreateRepository(repository.KMPSearch)
-		result, err := services.NewPatternSearchService(repo).SearchInDirectory(directory, word)
+		uc := usecase.NewCreatePatternSearchInteractor(
+			repo,
+			presenter.NewCreatePatternSearchPresenter(),
+			0,
+		)
+		output, err := uc.Execute(cmd.Context(), input)
 		if err != nil {
 			logger.WithError(err).Fatalln("Error while searching for pattern.")
 		}
-		logger.Infof("count: %v", result.Count)
+		logger.Infof("count: %v", output.Count)
 	},
 }
